56440ccc64c85763621526ad298def82cb51bb8c
lancubal <lunalancuba.sistemas@gmail.com>
2026-08-03 11:27:06 -0300

    fix: resolve prisma migrate error P3005 in docker-compose command with automatic baseline fallback

--- dashboard/.next/static/chunks/webpack.js
+++ dashboard/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6fd9541c6a1dd78a"; }
+/******/ 		__webpack_require__.h = function() { return "d2c82fa60f0aada4"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
